6c87d99b0047ec8904c480d38e44294390a5641a
CazYokoyama <caz@caztech.com>
2013-01-17 18:42:14 -0800

    integrate a test file which Steven Mason wrote.

---
+++ TurnPts_NP.js
@@ -0,0 +1,24 @@
+var latlo = new Array(
+'LatLon',
+'45.60667,-123.03167',
+'45.55333,-123.16',
+'45.85167,-123.24139',
+'44.86667,-123.19833',
+);
+
+
+var tpdesc = new Array(
+'Tpdesc',
+'NORTH PLAINS',
+'David Hill',
+'Vernonia',
+'Independence',);
+
+
+
+var tpn = new Array (
+'TriGr',
+'NP1',
+'DAH',
+'VER',
+'IND');
