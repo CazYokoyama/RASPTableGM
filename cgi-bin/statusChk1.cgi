2b31da8b92f8bf00a325c6e2f6422128afd70e7c
Caz Yokoyama <caz@caztech.com>
2013-12-24 19:59:07 -0800

    not used rasp chart.

--- cgi-bin/statusChk1.cgi
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#ssh -F /var/www/.ssh/config -v rasp@172.16.10.2 /home/rasp/RASP/RUN/status.sh 2>&1 > /tmp/status.out
-ssh 172.16.10.2 /home/rasp/RASP/RUN/status1.sh
--- cgi-bin/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-perl get_rasptrackavg.cgi region=uk latlons=45.60667,-123.03167,45.58333,-123.05333,45.64778,-123.06805 day=0 time=1200 polar=DiscusA wgt=1 tsink=1.0 tmult=1 grid=d2
